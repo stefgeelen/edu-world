90c0ab85a7638bd20d5bfaffa76b628b5e85694a
Stef Geelen <geelenstef001@gmail.com>
2026-06-10 07:48:57 +0200

    Fix correct voice

--- supabase/functions/synthesize-speech/index.ts
+++ supabase/functions/synthesize-speech/index.ts
@@ -41,8 +41,8 @@ serve(async (req) => {
         },
         body: JSON.stringify({
           text,
-          model_id: "eleven_multilingual_v2",
-          language_code: "nl-BE",
+          model_id: "eleven_turbo_v2_5",
+          language_code: "nl",
           voice_settings: {
             stability: 0.5,
             similarity_boost: 0.75,
